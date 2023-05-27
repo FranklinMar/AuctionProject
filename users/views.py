f60a081b80a7b8554961e816ef9f90920fe6e7f3
DenysBerezniuk <49430756+FranklinMar@users.noreply.github.com>
2023-05-27 21:24:08 +0300

    fix(views): Reconfigured views.py files to match PIP8 convention

--- users/views.py
+++ users/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 def user_id(request, id :str):
     return HttpResponse("<h1>Your id is "+id+"<h1>")
+
 
 def settings(request, id :str):
     return HttpResponse("<h1>settings of user: "+id+"<h1>")
