ec92bd878ab7c054e0c94c1ebccf4d7f8b3d776f
Dema <67798322+DemaReaktor@users.noreply.github.com>
2023-05-29 21:06:27 +0300

    Save changes

--- chats/views.py
+++ chats/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
+from main.models import Chat
 # Create your views here.
 
 def chat_id(request, id :str):
-    return HttpResponse("<h1>chat: "+id+"<h1>")
+        chat = Chat.find_one({"_id": id})
+        if not (chat is None):
+            for user in chat.users_list():
+                if 'name' in request.session and user.name == request.session['name']:
+                    return HttpResponse("<h1>chat id:"+chat.id+"</p>")
+        return HttpResponse("<h1>Error<h1>")
--- users/views.py
+++ users/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from main.models import User
+from pymongo import MongoClient
+from AuctionProject.secret import CONNECTION_STRING
+from AuctionProject.secret import CONNECTION_STRING
 
 # Create your views here.
 
 def user_id(request, id :str):
-    return HttpResponse("<h1>Your id is "+id+"<h1>")
-
+    d = MongoClient(CONNECTION_STRING)['Auction']['User'].find_one({"name":id},  no_cursor_timeout=True)
+    print(d)
+    user = User.find_one({"name": id})
+    print(3)
+    if user is None:
+        return HttpResponse("<h1>Error<h1>")
+    return HttpResponse("<h1>id:"+user.id+"<h1><p>name:"+user.name+"</p>")
 
 def settings(request, id :str):
-    return HttpResponse("<h1>settings of user: "+id+"<h1>")
+    if 'name' in request.session and request.session['name'] == id:
+        user = User.find_one({"name": id})
+        if not (user is None):
+            return HttpResponse("<h1>id:" + user.id + "<h1><p>name:" + user.name + ", settings</p>")
+    return HttpResponse("<h1>Error<h1>")
