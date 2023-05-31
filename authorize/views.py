cc596aa71e220eb59ba2de7f96e5ad48784a6ca7
Дмитро Дем'яник <67798322+DemaReaktor@users.noreply.github.com>
2023-06-01 00:39:53 +0300

    Update views.py

--- authorize/views.py
+++ authorize/views.py
@@ -5,8 +5,6 @@ from authorize.models import *
 from django.views.decorators.cache import never_cache
 from django.http import HttpResponseRedirect
 
-<<<<<<< HEAD
-
 @never_cache
 def login(request):
     if request.method == 'POST':
@@ -20,24 +18,6 @@ def login(request):
             return render(request, 'main/signin.html', {'back': request.POST.get('back', ''), 'form': Login(),
                                                       'error': 'Неправильний пароль або логін'})
     return render(request, 'main/signin.html', {'back': request.POST.get('back', ''), 'form': Login()})
-
-=======
-
-@never_cache
-def login(request):
-    if request.method == 'POST':
-        form = Login(request.POST)
-        if form.is_valid():
-            user = User.find_one({'name': form.cleaned_data['login']})
-            if not (user is None) and user.password == make_password(form.cleaned_data['password']):
-                request.session['name'] = user.name
-                request.session['image'] = user.image
-                return HttpResponseRedirect(request.POST.get('back', ''))
-            return render(request, 'main/signin.html', {'back': request.POST.get('back', ''), 'form': Login(),
-                                                      'error': 'Неправильний пароль або логін'})
-    return render(request, 'main/signin.html', {'back': request.POST.get('back', ''), 'form': Login()})
-
->>>>>>> master
 
 def logout(request):
     request.session.pop('name')
@@ -58,8 +38,4 @@ def create_user(request):
             except ValueError as error:
                 return render(request, 'main/signup.html', {'back': request.POST.get('back', ''),
                                             'form': CreateUser(), 'error': error})
-<<<<<<< HEAD
     return render(request, 'main/signup.html', {'back': request.POST.get('back', ''), 'form': CreateUser()})
-=======
-    return render(request, 'main/signup.html', {'back': request.POST.get('back', ''), 'form': CreateUser()})
->>>>>>> master
