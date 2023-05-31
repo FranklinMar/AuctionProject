5316ced8abee3a6d9627c18faae720821796d68a
Dema <67798322+DemaReaktor@users.noreply.github.com>
2023-05-31 20:23:07 +0300

    Update

--- authorize/models.py
+++ authorize/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.forms import Form, CharField, PasswordInput
+from django.forms import Form, CharField, EmailField, PasswordInput, EmailInput, TextInput
+
 
 class Login(Form):
     login = CharField(label='Email or Name', widget=TextInput(attrs={
@@ -36,11 +37,6 @@ class CreateUser(Form):
         'placeholder': 'Confirm Password'
     }), required=True)
 
-class Create_user(Form):
-    login = CharField(label = 'login')
-    password = CharField(label = 'пароль',widget=PasswordInput)
-    email = CharField(label = 'email')
-    image = CharField(label='фото', required=False)
     # image = FileField(allow_empty_file=False, widget=FileInput(attrs={
     #             'accept': '.jpg, .svg, .png, .gif',
     #             'id': 'file'
--- authorize/views.py
+++ authorize/views.py
@@ -1,41 +1,43 @@
+from django.shortcuts import render
 from main.models import User
+from django.contrib.auth.hashers import make_password
 from authorize.models import *
 from django.views.decorators.cache import never_cache
 from django.http import HttpResponseRedirect
-from django.contrib.auth.hashers import make_password
-from django.shortcuts import render
+
 
 @never_cache
 def login(request):
     if request.method == 'POST':
         form = Login(request.POST)
         if form.is_valid():
-            user = User.find_one({'name':form.cleaned_data['login']})
+            user = User.find_one({'name': form.cleaned_data['login']})
             if not (user is None) and user.password == make_password(form.cleaned_data['password']):
                 request.session['name'] = user.name
                 request.session['image'] = user.image
-                return HttpResponseRedirect(request.POST.get('back',''))
-            return render(request, 'main/login.html',{'back': request.POST.get('back',''), 'Login':Login(),\
-                                                      'error': 'неправильний пароль або логін'})
-    return render(request, 'main/login.html', {'back': request.POST.get('back',''), 'Login':Login()})
+                return HttpResponseRedirect(request.POST.get('back', ''))
+            return render(request, 'main/signin.html', {'back': request.POST.get('back', ''), 'form': Login(),
+                                                      'error': 'Неправильний пароль або логін'})
+    return render(request, 'main/signin.html', {'back': request.POST.get('back', ''), 'form': Login()})
+
 
 def logout(request):
     request.session.pop('name')
     request.session.pop('image')
     return HttpResponseRedirect('/')
 
+
 @never_cache
 def create_user(request):
-    form = Create_user(request.POST)
-    if form.is_valid():
-        try:
-            user = User.create(form.cleaned_data['login'],form.cleaned_data['password'],form.cleaned_data['email'],
-                               image=form.cleaned_data['image'])
-            request.session['name'] = user.name
-            request.session['image'] = user.image
-            return HttpResponseRedirect(request.POST.get('back', ''))
-        except ValueError as error:
-            return render(request, 'main/create_user.html', {'back': request.POST.get('back', ''),
-                                                             'create': Create_user(),
-                                                             'error': error})
-    return render(request, 'main/create_user.html', {'back': request.POST.get('back', ''), 'create': Create_user()})
+    if request.method == 'POST':
+        form = CreateUser(request.POST)
+        if form.is_valid():
+            try:
+                user = User.create(form.cleaned_data['login'], form.cleaned_data['password'],
+                                   form.cleaned_data['email'])  # , image=form.cleaned_data['image'])
+                request.session['name'] = user.name
+                return HttpResponseRedirect(request.POST.get('back', ''))
+            except ValueError as error:
+                return render(request, 'main/signup.html', {'back': request.POST.get('back', ''),
+                                            'form': CreateUser(), 'error': error})
+    return render(request, 'main/signup.html', {'back': request.POST.get('back', ''), 'form': CreateUser()})
