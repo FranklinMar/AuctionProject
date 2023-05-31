1e237df34c8097c8a733295fa766937212220cd5
Dema <67798322+DemaReaktor@users.noreply.github.com>
2023-05-31 19:38:57 +0300

    Chat done

--- chats/views.py
+++ chats/views.py
@@ -1,13 +1,19 @@
 from django.http import HttpResponse
-from main.models import Chat
+from main.models import Chat, User, Message
 from bson.objectid import ObjectId
 from django.shortcuts import render
 
 def chat_id(request, id :str):
-    chat = Chat.find_one({"_id": ObjectId(id)})
-    if not (chat is None):
-        if ('name' in request.session and chat.user1.name == request.session['name']) or \
-        ('name' in request.session and chat.user1.name == request.session['name']):
-            return render(request,'main/chat.html',{'chat':chat,'user2':
-                chat.user1 if chat.user2.name == request.session['name'] else chat.user2})
+    user2 = User.find_one({"name": id})
+    if not (user2 is None) and 'name' in request.session:
+        user1 = User.find_one({"name": request.session['name']})
+        chat = Chat.find_one_by_users(user1,user2)
+        if request.method == 'POST':
+            chat.send(Message.create(user1,request.POST.get('text', '')))
+        if chat is None:
+            chat = Chat.create(user1,user2)
+        for message in chat.messages:
+            print(message.user)
+        return render(request,'main/chat.html',{'chat':chat,'user2': chat.user1 \
+            if chat.user2.name == request.session['name'] else chat.user2})
     return HttpResponse("<h1>Error<h1>")
